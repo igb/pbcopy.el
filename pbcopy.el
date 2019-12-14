8fcc44c2a0ce62a2a7fac60081b90d5774bf4fca
ibrown <ibrown@twitter.com>
2019-12-14 13:50:17 -0800

    added escapes for most normal cases

--- pbcopy.el
+++ pbcopy.el
@@ -1,6 +1,14 @@
 (defun pbcopy ()
   "Copy contents of current buffer to the MacOS clipboard."
   (interactive)
-  (shell-command (concat "echo \"" (buffer-substring-no-properties 1 (buffer-size)) "\" | pbcopy"))
-  (message "Copied buffer to MacOS clipboard.")  
-  )
+  (setq content (escape (buffer-substring-no-properties 1 (buffer-size))))
+  (shell-command (concat "echo -e \"" content  "\" | pbcopy"))
+  (message (concat "Copied buffer \"" (buffer-name) "\" to MacOS clipboard.")))
+
+
+(defun escape(x) 
+ (replace-regexp-in-string "\n+" "\\\\n"   
+     (replace-regexp-in-string "\"+" "\\\\\"" 
+	  (replace-regexp-in-string "\\\\+" "\\\\\\\\" x))))
+						    
+
